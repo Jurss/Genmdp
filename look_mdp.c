1f7c1e262b329c67839f54975c3259926a28dcdc
Rémi Dutombois <remi62120@hotmail.fr>
2020-12-16 19:37:42 +0100

    Ajout recherche mdp unique

--- look_mdp.c
+++ look_mdp.c
@@ -8,9 +8,13 @@ void look_mdp(void)
 	c = fgetc(stdin);
 	if (c == '1')
 	{
+		char want[15];
 		viderBuffer();
-		
 
+		printf("Pour quel compte ?\n");
+		ft_fgets(want, 15);
+		
+		wanted_pass(want);
 	}
 	else if (c == '2')
 	{
@@ -24,4 +28,35 @@ void look_mdp(void)
 		aescrypt();
 		fclose(fichier);
 	}
+}
+
+int wanted_pass(char *rec)
+{
+	system("sudo chattr -i mdp.txt.aes");
+	aesdecrypt();
+        FILE* compte = fopen("mdp.txt","r");
+        char line[25];
+        char wanted[25];
+        int len;
+
+        while (fgets(line,sizeof line, compte))                 //recherche rec
+        {
+                if (strstr(line, rec))
+                {
+                        len = strlen(line);                     //
+                        fseek(compte, -len, SEEK_CUR);          //
+                        fgets(wanted, sizeof wanted, compte);   //reviens au debut de la ligne
+                        printf("%s", wanted);                   //et l'affiche
+                        break;                                  //
+                }
+
+        }
+        if (feof(compte))
+        {
+                printf("Ce compte est introuvable ! Verifier l'ortographe de celui-ci.\n");
+        }
+	aescrypt();
+	system("sudo chattr +i mdp.txt.aes");
+        fclose(compte);
+        return (1);
 }
--- look_mdp.h
+++ look_mdp.h
@@ -3,9 +3,12 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+int ft_fgets(char *chaine, int longueur);
 void look_mdp(void);
 void viderBuffer(void);
 void aesdecrypt(void);
 void aescrypt(void);
+int wanted_pass(char *rec);
 #endif
